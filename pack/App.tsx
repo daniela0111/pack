44c483681e0b0ffe99d593a8551407599647bfbc
daniela0111 <95025799+daniela0111@users.noreply.github.com>
2024-08-28 10:32:18 +0200

    Homepage
    editing homepage

--- pack/App.tsx
+++ pack/App.tsx
@@ -1,35 +1,24 @@
-import * as React from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator} from '@react-navigation/bottom-tabs' ;
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomePage() {
-  return(
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  )
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import HomePage from '././screens/HomePage';
+import SettingsScreen from '././screens/SettingScreen';
+import PhotoScreen from '././screens/PhotoScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomePage} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
-  </NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="Camera" component={PhotoScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
--- pack/screens/HomePage.tsx
+++ pack/screens/HomePage.tsx
@@ -1,12 +1,42 @@
-import React from "react";
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, Button } from 'react-native';
 
-const HomeSreen = () => {
-    return (
-        <View>
-            <Text>Home Screen</Text>
+export default function HomePage() {
+  return (
+    <View style={{ flex: 4, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Main navigation</Text>
+      <View>
+        <Button
+          title="Doklady přijaté"
+          accessibilityLabel="increment"
+          color="#060663"
+          onPress={() => console.log('Doklady přijaté pressed')}
+        />
         </View>
-        );
-};
-
-export default HomeSreen;
+        <View>
+        <Button
+          title="Doklady vydané"
+          accessibilityLabel="increment"
+          color="#060663"
+          onPress={() => console.log('Doklady přijaté pressed')}
+        />
+        </View>
+        <View>
+        <Button
+          title="Účtenky"
+          accessibilityLabel="increment"
+          color="#060663"
+          onPress={() => console.log('Doklady přijaté pressed')}
+        />
+        </View>
+        <View>
+        <Button
+          title="Ostatní dokumenty"
+          accessibilityLabel="increment"
+          color="#060663"
+          onPress={() => console.log('Doklady přijaté pressed')}
+        />
+        </View>
+    </View>
+  );
+}
